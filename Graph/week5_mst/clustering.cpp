3f4863b2b929d1513ca21dfaa1bcad0819f8d435
ahmedalam782 <63088248+ahmedalam782@users.noreply.github.com>
2020-04-28 08:23:50 +0200

    Add files via upload

--- Graph/week5_mst/clustering.cpp
+++ Graph/week5_mst/clustering.cpp
@@ -1,16 +1,17 @@
 #include <algorithm>
+#include <cassert>
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <vector>
 
 using std::vector;
+using std::pair;
 struct Node {
   int start, end;
   double dis;
   Node(int x, int y, double d) : start(x), end(y), dis(d){};
   bool operator<(const Node &a) const { return dis < a.dis; }
-
 };
 int Find(int x, vector<int> &parent) {
   if (x != parent[x])
@@ -24,11 +25,13 @@ void Union(int x, int y, vector<int> &parent) {
     parent[right] = left;
   }
 }
-double minimum_distance(vector<int> x, vector<int> y) {
-  double result = 0.;
+
+double clustering(vector<int> x, vector<int> y, int k) {
+  // write your code here
+  int num = 0;
   vector<int> parent(x.size());
-  vector<Node> q;                     
-  for (size_t i = 0; i < x.size(); i++) 
+  vector<Node> q;                       //??????????
+  for (size_t i = 0; i < x.size(); i++) //??????
     parent[i] = i;
   for (size_t i = 0; i < x.size(); i++) {
     for (size_t j = i + 1; j < x.size(); j++) {
@@ -43,20 +46,23 @@ double minimum_distance(vector<int> x, vector<int> y) {
     Node v = q[i];
     if (Find(v.start, parent) !=
         Find(v.end, parent)) { 
-      result += v.dis;         
+      if (num == x.size() - k) 
+        return v.dis;
+      num++;                        
       Union(v.start, v.end, parent); 
     }
   }
-  return result;
 }
 
 int main() {
   size_t n;
+  int k;
   std::cin >> n;
   vector<int> x(n), y(n);
   for (size_t i = 0; i < n; i++) {
     std::cin >> x[i] >> y[i];
   }
-  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+  std::cin >> k;
+  std::cout << std::setprecision(10) << clustering(x, y, k) << std::endl;
   return 0;
 }
